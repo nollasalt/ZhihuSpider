09286e87a357d76d4b1d8578cc4191a688230a2a
Calccx <allcalc@outlook.com>
2022-10-16 19:33:07 +0800

    增加示例

--- 知乎爬虫.py
+++ 知乎爬虫.py
@@ -5,7 +5,7 @@ import datetime
 import openpyxl
 import os
 sched = BlockingScheduler()
-
+#定时任务
 @sched.scheduled_job('interval', hours=1)
 def timed_job():
     headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
